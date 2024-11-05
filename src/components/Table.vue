73331855dd23c5952576b62e487077b9af387b74
RDG-Sanchez <rafaelzhx@gmail.com>
2024-11-04 22:59:29 -0300

    add functions for get data from PokeAPI, component Table now renders data of PokeAPI

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,7 +1,66 @@
-<script setup></script>
+<script setup>
+import { onMounted, ref } from "vue";
+import axios from "axios";
+
+const isLoading = ref(false);
+const pokemons = ref([]);
+
+const getPokemons = async () => {
+  isLoading.value = true;
+  try {
+    const {
+      data: { results },
+    } = await axios.get("https://pokeapi.co/api/v2/pokemon/?limit=50");
+
+    const promises = await results.map(async ({ url }) => {
+      const { data } = await axios.get(url);
+      return {
+        id: data.id,
+        name: data.name,
+        type: data.types[0].type.name,
+        sprite: data.sprites.front_default,
+      };
+    });
+
+    pokemons.value = await Promise.all(promises);
+    isLoading.value = false;
+  } catch (error) {
+    alert("Ha ocurrido un error");
+  }
+};
+
+const handleData = (data) => {
+  console.log(data);
+};
+
+onMounted(() => {
+  getPokemons();
+});
+</script>
 
 <template>
-  <div><h1>Esto es una tabla</h1></div>
+  <span v-if="isLoading === true">Cargando...</span>
+  <DataTable
+    v-else
+    :value="pokemons"
+    paginator
+    :rows="5"
+    :rowsPerPageOptions="[5, 10, 15, 20, 25]"
+  >
+    <Column field="id" header="ID" style="width: 25%"></Column>
+    <Column field="name" header="Nombre" style="width: 25%"></Column>
+    <Column field="type" header="Tipo" style="width: 25%"></Column>
+    <Column header="Acciones" style="width: 25%">
+      <template #body="{ data }">
+        <Button
+          icon="pi pi-eye"
+          @click="handleData(data)"
+          severity="secondary"
+          rounded
+        ></Button>
+      </template>
+    </Column>
+  </DataTable>
 </template>
 
 <style scoped></style>
