43e8e3dc3bc28f94b89621fe4fabcbc61f38ec83
RDG-Sanchez <rafaelzhx@gmail.com>
2024-11-04 22:23:23 -0300

    add PrimeVue, PrimeIcons, PrimeVueComponents as GlobalComponents and other minor changes

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import "primeicons/primeicons.css";
+
+import Table from "./components/Table.vue";
+</script>
+
+<template>
+  <div><Table /></div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Table.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div><h1>Esto es una tabla</h1></div>
+</template>
+
+<style scoped></style>
