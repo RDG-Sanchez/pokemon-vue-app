00904c1cb13cdc5c84c2c6ac804c4ed27e10a6eb
RDG-Sanchez <rafaelzhx@gmail.com>
2024-11-05 16:29:20 -0300

    deleted items of Navbar component

--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -4,28 +4,6 @@
       <template #start>
         <h1 aria-label="PokeAPP" class="font-bold text-2xl">PokeAPP</h1>
       </template>
-      <template #item="{ item, props, hasSubmenu, root }">
-        <a v-ripple class="flex items-center" v-bind="props.action">
-          <span>{{ item.label }}</span>
-          <Badge
-            v-if="item.badge"
-            :class="{ 'ml-auto': !root, 'ml-2': root }"
-            :value="item.badge"
-          />
-          <span
-            v-if="item.shortcut"
-            class="ml-auto border border-surface rounded bg-emphasis text-muted-color text-xs p-1"
-            >{{ item.shortcut }}</span
-          >
-          <i
-            v-if="hasSubmenu"
-            :class="[
-              'pi pi-angle-down ml-auto',
-              { 'pi-angle-down': root, 'pi-angle-right': !root },
-            ]"
-          ></i>
-        </a>
-      </template>
       <template #end>
         <div class="flex items-center gap-2">
           <Avatar
